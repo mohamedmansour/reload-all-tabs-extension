87e6a15282121d637f6d0208a4d5f5b249bdd0f3
Mohamed <hello@mohamedmansour.com>
2009-10-03 13:56:49 -0400

    Cleanup manifest and remove unwanted logging

--- bookmark_all_tabs/keyboard_handler.js
+++ bookmark_all_tabs/keyboard_handler.js
@@ -1,6 +1,5 @@
 // Mohamed Mansour 2009 (hello@mohamedmansour.com)
 
-
 // Add a keyboard listener on keyup.
 if (window == top) {
   window.addEventListener('keyup', keyListener, false);
@@ -9,7 +8,6 @@ if (window == top) {
 // Keyboard keyup listener callback.
 function keyListener(e) {	
  if (e.ctrlKey && e.shiftKey && e.which == 68) {
-    console.log("Pressed button After");
     var folderName = prompt("Please enter a folder name", "My bookmarks");
     if (folderName) {
       // Notify the extension that we want to bookmark all tabs.
