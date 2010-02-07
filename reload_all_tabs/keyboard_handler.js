6511cb57cfc1d12c7d0f71932e48fbacfcccffc2
Mohamed <hello@mohamedmansour.com>
2010-02-07 14:51:58 -0500

    Reorgnize the messaging api so that we can handle every outside extension request.

--- reload_all_tabs/keyboard_handler.js
+++ reload_all_tabs/keyboard_handler.js
@@ -5,9 +5,12 @@ if (window == top) {
   window.addEventListener("keyup", keyListener, false);
 }
 
-// Keyboard keyup listener callback.
+/**
+* Keyboard keyup listener callback.
+*/
 function keyListener(e) {
-  // Must press ctrl key to validate.
+  // Must press ctrl key to validate. Filter the keys if the keyCode is Shift/Ctrl/Alt since we are
+  // capturing it via its own modifier.
   if (e.ctrlKey && e.keyCode && !e.metaKey && e.keyCode != 16 && e.keyCode != 17 && e.keyCode != 18) {
     chrome.extension.sendRequest({
       code: e.keyCode,
