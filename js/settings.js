1540d663bf93c4e6a34e77873b86282d93db8f1f
Mohamed <hello@mohamedmansour.com>
2011-05-07 13:12:18 -0400

    Order getters/setters alphabetically.

--- js/settings.js
+++ js/settings.js
@@ -1,17 +1,23 @@
 // Settings for reload all tabs.
 settings = {
+  get contextMenu() {
+    var key = localStorage['context_menu'];
+    return (typeof key == 'undefined') ? true : key === 'true';
+  },
+  set contextMenu(val) {
+    localStorage['context_menu'] = val;
+  },
+  get firstRun() {
+    return localStorage['first_run'];
+  },
+  set firstRun(val) {
+    localStorage['first_run'] = val;
+  },
   get reloadAllWindows() {
     return localStorage['reload_all_windows'] === 'true';
   },
   set reloadAllWindows(val) {
     localStorage['reload_all_windows'] = val;
-  },
-  get shortcutKeyCode() {
-    var key = localStorage['shortcut_key_code'];
-    return (typeof key == 'undefined') ? 82 : key;
-  },
-  set shortcutKeyCode(val) {
-    localStorage['shortcut_key_code'] = val;
   },
   get shortcutKeyAlt() {
     var key = localStorage['shortcut_key_alt'];
@@ -19,6 +25,13 @@ settings = {
   },
   set shortcutKeyAlt(val) {
     localStorage['shortcut_key_alt'] = val;
+  },
+  get shortcutKeyCode() {
+    var key = localStorage['shortcut_key_code'];
+    return (typeof key == 'undefined') ? 82 : key;
+  },
+  set shortcutKeyCode(val) {
+    localStorage['shortcut_key_code'] = val;
   },
   get shortcutKeyShift() {
     var key = localStorage['shortcut_key_shift'];
@@ -32,18 +45,5 @@ settings = {
   },
   set version(val) {
     localStorage['version'] = val;
-  },
-  get firstRun() {
-    return localStorage['first_run'];
-  },
-  set firstRun(val) {
-    localStorage['first_run'] = val;
-  },
-  get contextMenu() {
-    var key = localStorage['context_menu'];
-    return (typeof key == 'undefined') ? true : key === 'true';
-  },
-  set contextMenu(val) {
-    localStorage['context_menu'] = val;
   }
 };
