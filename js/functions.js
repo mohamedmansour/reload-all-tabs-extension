82c41e884fdef6c952b289ff7b8b071d8c3e62a6
Mohamed <hello@mohamedmansour.com>
2011-05-07 13:10:46 -0400

    Remove Bind since it is now implemented natively in Chrome

--- js/functions.js
+++ js/functions.js
@@ -5,18 +5,3 @@
 function $(id) {
   return document.getElementById(id);
 }
-
-/**
- * Build own bind function that exposes all functions.
- * From Robert Sosinski
- *  http://www.robertsosinski.com/2009/04/28/binding-scope-in-javascript/
- *
- * @param {class} scope The class to bind this function to.
- * @return {function} The scoped function binded.
- */
-Function.prototype.bind = function(scope) {
-  var _function = this;
-  return function() {
-    return _function.apply(scope, arguments);
-  }
-};
